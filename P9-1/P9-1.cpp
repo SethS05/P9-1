30ff396f5f5e146d141d851f6207acd9829f2015
SethS05 <s-skinner@onu.edu>
2025-04-03 00:21:46 -0400

    Finished 9-1

--- P9-1/P9-1.cpp
+++ P9-1/P9-1.cpp
@@ -8,7 +8,7 @@
 * all elements in reverse order, and only the first and last element.
 */
 
-#include<iostream>
+#include <iostream>
 #include <cstdlib>
 #include <ctime>
 
@@ -17,21 +17,24 @@ using namespace std;
 void getEvenIndexElements(int arr1[], int size, int evenIndexArray[], int& evenIndexSize);
 void findEvenElements(int arr1[], int size, int evenArray[], int& evenSize);
 
-int main()
-{
+
+int main() {
+
 	int arr1[10];
 	int evenIndexArray[10];
 	int evenIndexSize;
 	int evenArray[10];
 	int evenSize;
 
-	srand(time(0));
+	int const size = 10;
 
-	const int size = 10;
+	// Initialize random values for the array.
+	srand(time(0));
 
 	cout << "The random integers: ";
 
-	for (int i = 0; i < size; i++) {
+	for (int i = 0; i < size; i++) 
+	{
 		arr1[i] = 1000 + rand() % 9000;
 		cout << arr1[i] << " ";
 	}
@@ -41,7 +44,8 @@ int main()
 
 	cout << "\nEven Indices: ";
 
-	for (int i = 0; i < evenIndexSize; i++) {
+	for (int i = 0; i < evenIndexSize; i++)
+	{
 		cout << evenIndexArray[i] << " ";
 	}
 
@@ -49,13 +53,15 @@ int main()
 	findEvenElements(arr1, size, evenArray, evenSize);
 
 	cout << "\nEven values: ";
-	for (int i = 0; i < evenSize; i++) {
+	for (int i = 0; i < evenSize; i++)
+	{
 		cout << evenArray[i] << " ";
 	}
 
 	// Print the arr1[] elements in reverse order
 	cout << "\nReversed Order: ";
-	for (int i = size - 1; i >= 0; i--) {
+	for (int i = size - 1; i >= 0; i--)
+	{
 		cout << arr1[i] << " ";
 	}
 
@@ -64,12 +70,12 @@ int main()
 	cout << arr1[0] << " " << arr1[size - 1];
 }
 
-}
-
 void getEvenIndexElements(int arr1[], int size, int evenIndexArray[], int& evenIndexSize) {
 	evenIndexSize = 0;
-	for (int i = 0; i < size; i++) {
-		if (i % 2 == 0) {
+	for (int i = 0; i < size; i++)
+	{
+		if (i % 2 == 0) 
+		{
 			evenIndexArray[evenIndexSize++] = arr1[i];
 		}
 	}
@@ -77,11 +83,11 @@ void getEvenIndexElements(int arr1[], int size, int evenIndexArray[], int& evenI
 
 void findEvenElements(int arr1[], int size, int evenArray[], int& evenSize) {
 	evenSize = 0;
-	for (int i = 0; i < size; i++) {
-		if (arr1[i] % 2 == 0) {
+	for (int i = 0; i < size; i++) 
+	{
+		if (arr1[i] % 2 == 0) 
+		{
 			evenArray[evenSize++] = arr1[i];
 		}
 	}
 }
-
- 
