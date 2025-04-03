ef419349d54ff65559f01a56f16069ac2095b15a
SethS05 <s-skinner@onu.edu>
2025-04-02 23:56:30 -0400

    Add project files.

---
+++ P9-1/P9-1.cpp
@@ -0,0 +1,20 @@
+/*
+* Author: Seth Skinner
+* File: P9-1
+* Date: 4/2/2025
+* 
+* Description: initializes an array with ten 4-digit random integers and then prints five lines of output,
+* containing every element in order of appearance in the array, every element at an even index, every even element,
+* all elements in reverse order, and only the first and last element.
+*/
+
+#include<iostream>
+
+using namespace std;
+
+main()
+{
+
+
+}
+
